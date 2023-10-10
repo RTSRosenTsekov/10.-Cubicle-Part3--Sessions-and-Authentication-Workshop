d46ec06c656b3eb6027bde9d5ab1cb8d96d3112e
Rosen.Tsekov <ros1914@yahoo.com>
2023-10-10 19:05:24 +0300

    implement login logic with bcrypt comparing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,27 @@
 const router = require("express").Router();
 const userService = require("../services/userService");
 
-router.get("/register",(req,res)=>{
-    res.render("user/register");
+router.get("/register", (req, res) => {
+  res.render("user/register");
 });
 
-router.post("/register", async (req,res)=>{
-   
-    const {username,password,repeatPassword}=req.body;
-    await userService.register({username,password,repeatPassword});
+router.post("/register", async (req, res) => {
+  const { username, password, repeatPassword } = req.body;
+  await userService.register({ username, password, repeatPassword });
 
-    res.redirect("/users/login");
-})
-
-
-router.get("/login", (req,res)=>{
-    res.render("user/login");
+  res.redirect("/users/login");
 });
 
+router.get("/login", (req, res) => {
+  res.render("user/login");
+});
 
-module.exports=router;
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await userService.login(username, password);
+
+  console.log(user);
+  res.redirect("/");
+});
+
+module.exports = router;
