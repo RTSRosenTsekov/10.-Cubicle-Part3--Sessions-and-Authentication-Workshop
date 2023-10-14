f9a9b0fc706b9a6905f862ae8aab2c607083edfb
Rosen.Tsekov <ros1914@yahoo.com>
2023-10-14 09:54:32 +0300

    provide global variables for hbs template with res.locals

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -5,8 +5,10 @@ exports.auth =async (req, res,next) => {
   const token = req.cookies["auth"];
   if(token){
       try {
-        const decodetToken = await jwt.verify(token,SECRET);
-        req.user = decodetToken;
+        const decodedToken = await jwt.verify(token,SECRET);
+        req.user = decodedToken;
+        res.locals.user = decodedToken;
+        res.locals.isAuthenticated = true;
         
         next();
 
